9a3cfced55a677c21438aad8cfef42a37146a36e
marciomdavid <136757539+marciomdavid@users.noreply.github.com>
2023-07-19 16:49:31 +0700

    Refactor file structure

---
+++ mountain_nature_house.py
@@ -0,0 +1,422 @@
+1. class AnimalWelfareWarriors:
+	def __init__(self):
+		self.animals = []
+		self.goals = []
+		self.actions = []
+
+2.	def add_animals(self, animal):
+		self.animals.append(animal)
+
+3.	def set_goals(self, goal):
+		self.goals.append(goal)
+
+4.	def take_action(self, action):
+		self.actions.append(action)
+
+5.	def show_objectives(self):
+		print("The objectives of Animal Welfare Warriors are:")
+		for goal in self.goals:
+			print(goal)
+
+6.	def list_animals(self):
+		print("Animals we are fighting for are:")
+		for animal in self.animals:
+			print(animal)
+
+7.	def show_actions(self):
+		print("Our Actions are:")
+		for action in self.actions:
+			print(action)
+
+8.	def donate(self, amount):
+		print("Thank you for your generous donation of $" + str(amount))
+
+9.	def volunteer(self):
+		print("Thank you for volunteering your time to help animals in need")
+
+10.	def educate(self):
+		print("You are helping to educate others on animal welfare")
+
+11.	def advocate(self):
+		print("You are advocating for change to help animals")
+
+12.	def get_updates(self):
+		print("Keep up to date with our latest news")
+
+13.	def show_support(self):
+		print("Thank you for showing your support for Animal Welfare Warriors")
+
+14.	def spread_awareness(self):
+		print("You are helping to spread awareness of animal welfare issues")
+
+15.	def join_campaigns(self):
+		print("Join us in our campaigns to help animals")
+
+16.	def lead_campaigns(self):
+		print("Thank you for leading campaigns to help animals")
+
+17. def partner(self):
+		print("Thank you for partnering with Animal Welfare Warriors")
+
+18. def participate(self):
+		print("Thank you for participating in our work to help animals")
+
+19. def sign_petitions(self):
+		print("You are helping animals by signing petitions")
+
+20. def march(self):
+		print("March with us to help animals")
+
+21. def contact_representatives(self):
+		print("You are helping animals by contacting representatives")
+
+22. def start_dialogue(self):
+		print("You are helping to start the dialogue on animal welfare")
+
+23. def foster(self):
+		print("You are helping animals by fostering them")
+
+24. def adopt(self):
+		print("Thank you for adopting an animal in need")
+
+25. def rescue(self):
+		print("Thank you for rescuing animals in need")
+
+26. def fundraise(self):
+		print("Thank you for fundraising for animals in need")
+
+27. def donate_goods(self):
+		print("Thank you for donating goods to help animals")
+
+28. def join_events(self):
+		print("Join us in events to help animals")
+
+29. def help_shelters(self):
+		print("Help animal shelters in your community")
+
+30. def pick_up_litter(self):
+		print("Help the environment by picking up litter")
+
+31. def participate_conservation(self):
+		print("Help preserve the environment by participating in conservation efforts")
+
+32. def reduce_energy_consumption(self):
+		print("Help the environment by reducing energy consumption")
+
+33. def raise_awareness(self):
+		print("Raise awareness about animal welfare issues")
+
+34. def donate_time(self):
+		print("Donate your time and energy to help animals")
+
+35. def speak_out(self):
+		print("Speak out for animals who cannot speak for themselves")
+
+36. def report_cruelty(self):
+		print("Report animal cruelty when you see it")
+
+37. def join_groups(self):
+		print("Join groups that are fighting for animal welfare")
+
+38. def write_to_politicians(self):
+		print("Write to your politicians about animal welfare issues")
+
+39. def inspire_others(self):
+		print("Inspire others to take action for animal welfare")
+
+40. def sign_petitions(self):
+		print("Sign petitions to help animals")
+
+41. def create_petitions(self):
+		print("Create petitions to help animals")
+
+42. def start_movements(self):
+		print("Start movements to help animals")
+
+43. def challenge_injustice(self):
+		print("Challenge injustice for animals")
+
+44. def start_fundraisers(self):
+		print("Start fundraisers to support animal welfare")
+
+45. def organize_events(self):
+		print("Organize events to support animal welfare")
+
+46. def share_information(self):
+		print("Share information about animal welfare")
+
+47. def become_activists(self):
+		print("Become activists to protect animal rights")
+
+48. def provide_shelter(self):
+		print("Provide shelter for animals in need")
+
+49. def change_laws(self):
+		print("Change laws to protect animals")
+
+50. def support_organizations(self):
+		print("Support organizations that are fighting for animal welfare")
+
+51. def talk_to_people(self):
+		print("Talk to people about animal welfare")
+
+52. def influence_others(self):
+		print("Influence others to fight for animal welfare")
+
+53. def become_involved(self):
+		print("Become involved in animal welfare efforts")
+
+54. def donate_resources(self):
+		print("Donate resources to animal welfare organizations")
+
+55. def create_change(self):
+		print("Create change for the betterment of animals")
+
+56. def create_alternatives(self):
+		print("Create alternatives to animal testing")
+
+57. def resist_exploitation(self):
+		print("Resist exploitation of animals")
+
+58. def protest_cruelty(self):
+		print("Protest cruelty to animals")
+
+59. def campaign_change(self):
+		print("Campaign for change to protect animals")
+
+60. def value_lives(self):
+		print("Value the lives of animals")
+
+61. def stay_informed(self):
+		print("Stay informed about animal welfare issues")
+
+62. def promote_compassion(self):
+		print("Promote compassion for animals")
+
+63. def use_products_responsibly(self):
+		print("Use products responsibly to protect animals")
+
+64. def prevent_cruelty(self):
+		print("Prevent cruelty to animals")
+
+65. def defend_rights(self):
+		print("Defend the rights of animals")
+
+66. def support_legislation(self):
+		print("Support legislation to protect animals")
+
+67. def inform_others(self):
+		print("Inform others about animal welfare issues")
+
+68. def reduce_consumption(self):
+		print("Reduce consumption of animal products")
+
+69. def make_difference(self):
+		print("Make a difference in animal welfare")
+
+70. def join_movements(self):
+		print("Join movements to make a change in animal welfare")
+
+71. def raise_voices(self):
+		print("Raise our voices to end animal abuse")
+
+72. def shop_responsibly(self):
+		print("Shop responsibly to support animal welfare")
+
+73. def spread_word(self):
+		print("Spread the word about animal welfare")
+
+74. def speak_truth(self):
+		print("Speak out for the truth about animal welfare")
+
+75. def use_social_influence(self):
+		print("Use social influence to support animal welfare")
+
+76. def stop_animal_slavery(self):
+		print("Stop animal slavery and exploitation")
+
+77. def boycott_companies(self):
+		print("Boycott companies that contribute to animal cruelty")
+
+78. def start_conversations(self):
+		print("Start conversations about animal welfare")
+
+79. def take_stands(self):
+		print("Take a stand against animal cruelty")
+
+80. def gather_support(self):
+		print("Gather support for animal welfare")
+
+81. def invest_resources(self):
+		print("Invest in resources to help animals")
+
+82. def promote_wellness(self):
+		print("Promote wellness for animals")
+
+83. def end_hunting(self):
+		print("End hunting of endangered animals")
+
+84. def care_for_environments(self):
+		print("Care for the environments that animals inhabit")
+
+85. def challenge_accepted_norms(self):
+		print("Challenge accepted norms to help animals")
+
+86. def save_habitats(self):
+		print("Save habitats to protect animals")
+
+87. def end_trade(self):
+		print("End the trade of endangered animals")
+
+88. def end_experimentation(self):
+		print("End experimentation on animals")
+
+89. def create_solutions(self):
+		print("Create solutions to benefit animals")
+
+90. def stand_for_animals(self):
+		print("Stand up for animals who need our help")
+
+91. def fight_extinction(self):
+		print("Fight to prevent animal extinction")
+
+92. def stand_against_oppression(self):
+		print("Stand against the oppression of animals")
+
+93. def provide_protection(self):
+		print("Provide protection for animals")
+
+94. def restore_balance(self):
+		print("Restore balance for animals and their habitats")
+
+95. def end_exploitation(self):
+		print("End animal exploitation")
+		
+96. def end_abuse(self):
+		print("End animal abuse")
+
+97. def raise_standards(self):
+		print("Raise standards for animal welfare")
+
+98. def recognize_rights(self):
+		print("Recognize and respect the rights of animals")
+
+99. def combat_neglect(self):
+		print("Combat neglect of animal welfare issues")
+
+100. def support_sanctuaries(self):
+		print("Support sanctuaries that are protecting animals")
+
+101. def promote_change(self):
+		print("Promote change for animal welfare")
+
+102. def help_endangered(self):
+		print("Help protect endangered species")
+
+103. def reduce_pollution(self):
+		print("Reduce pollution to protect animals")
+
+104. def support_bans(self):
+		print("Support bans to protect animals")
+
+105. def fight_injustices(self):
+		print("Fight injustices against animals")
+
+106. def end_captivity(self):
+		print("End captivity of animals")
+
+107. def fundraise_shelters(self):
+		print("Fundraise for animal shelters")
+
+108. def support_welfare_organizations(self):
+		print("Support animal welfare organizations")
+
+109. def stop_factory_farming(self):
+		print("Stop factory farming of animals")
+
+110. def advocate_alternatives(self):
+		print("Advocate for alternatives to animal testing")
+
+111. def prevent_abuse(self):
+		print("Prevent abuse of animals")
+
+112. def end_overfishing(self):
+		print("End overfishing to protect marine animals")
+
+113. def end_trafficking(self):
+		print("End trafficking of animals")
+
+114. def protect_habitats(self):
+		print("Protect habitats of animals")
+
+115. def promote_awareness(self):
+		print("Promote awareness of animal welfare issues")
+
+116. def end_poaching(self):
+		print("End poaching of animals")
+
+117. def end_prize_fights(self):
+		print("End prize fights of animals")
+
+118. def boycott_abusive_businesses(self):
+		print("Boycott businesses that are abusive to animals")
+
+119. def end_cruelty(self):
+		print("End cruelty to animals")
+
+120. def promote_humane_treatment(self):
+		print("Promote humane treatment of animals")
+
+121. def reduce_plastic_use(self):
+		print("Reduce use of plastic to help animals")
+
+122. def end_unsustainable_practices(self):
+		print("End unsustainable practices that harm animals")
+
+123. def donate_animal_charities(self):
+		print("Donate to animal charities")
+
+124. def pressure_governments(self):
+		print("Pressure governments to act on animal welfare issues")
+
+125. def end_circuses(self):
+		print("End circuses that exploit animals")
+
+126. def speak_for_animals(self):
+		print("Speak for animals who cannot speak for themselves")
+
+127. def provide_sustenance(self):
+		print("Provide sustenance for animals in need")
+
+128. def provide_safety(self):
+		print("Provide safety for animals")
+
+129. def research_alternatives(self):
+		print("Research alternatives to animal testing")
+
+130. def end_entertainment_shows(self):
+		print("End entertainment shows that exploit animals")
+
+131. def promote_vegetarianism(self):
+		print("Promote vegetarianism to reduce animal consumption")
+
+132. def rescue_endangered(self):
+		print("Rescue endangered species")
+
+133. def stop_illegal_trade(self):
+		print("Stop illegal trade of animals")
+
+134. def support_veterinarians(self):
+		print("Support veterinarians to protect animals")
+
+135. def provide_care(self):
+		print("Provide care to animals in need")
+
+136. def end_fur_trade(self):
+		print("End the fur trade to protect animals")
+
+137. def create_protection_laws(self):
+		print("Create laws to protect animals")
+
+138. def focus_on_solutions(self):
+		print("Focus on solutions instead
